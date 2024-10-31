43270764f13e4e8826ca911d43c51a8c5d7735fd
Sean McGovern <srm5ka@virginia.edu>
2024-10-31 17:13:46 -0400

    Created ApiClient, DataManager, and implemented ETL process

--- CO2EmissionsETL/Program.cs
+++ CO2EmissionsETL/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace CO2EmissionsETL;
+﻿
+namespace CO2EmissionsETL;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
 
+    static async Task Main(string[] args)
+    {
+        bool success = false;
+        Console.WriteLine("Fetching emissions data from the World Bank API.");
+        var client = new WorldBankApiClient();
+        List<WorldBankEmissionsResponse> emissionsData = await client.FetchEmissionsDataForCountriesAsync();
+
+        // validate the emissions data returned
+        bool validApiResponse = true;
+        validApiResponse = emissionsData.Count > 0 && !emissionsData.Any(emissionsResponse => emissionsResponse.Data.Count == 0);
+
+        // if valid data was fetched, proceed to save the data to the database
+        if (validApiResponse)
+        {
+            Console.WriteLine("Successfully fetched emissions data.");
+            Console.WriteLine("Proceeding to save returned emissions data to the SQLite database.");
+
+            // Save the emissions data to the database
+            bool dbInitializationValid = EmissionsDataManager.InitializeDatabase();
+            if (dbInitializationValid)
+            {
+                success = EmissionsDataManager.InsertEmissionsData(emissionsData);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid emissions data was returned.");
+        }
+        Console.WriteLine($"Emissions data was {(success ? string.Empty : "not ")}successfully saved.");
+    }
+
+}
